a8da7250ef0206f60c70059a1d10887d96b04e91
ttsy <245452530@qq.com>
2018-11-10 17:15:33 +0800

    改成 2 缩进

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,47 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-    },
-    globals: {
-        '$': false,
-        'jQuery': false,
-    },
-    extends: [
-        // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-        'standard'
-    ],
-    // required to lint *.vue files
-    plugins: [
-        'html'
-    ],
-    // add your custom rules here
-    rules: {
-        // 不强制使用一致的缩进
-        'indent': 'off',
-        // 不强制使用一致的反勾号、双引号或单引号
-        'quotes': 'off',
-        // 不要求在语句末尾是否需要添加分号
-        'semi': 'off',
-        // 不要求一定要使用 === 和 !==
-        'eqeqeq': 'off',
-        // 不要求函数圆括号之前是否需要一个空格
-        'space-before-function-paren': 'off',
-        // 不要求语句块之前是否需要空格
-        'space-before-blocks': 'off',
-        // 不强制在关键字前后使用一致的空格
-        'keyword-spacing': 'off',
-        // 不强制在对象字面量的键和值之间使用一致的空格
-        'key-spacing': 'off',
-        // 允许行尾空格
-        'no-trailing-spaces': 'off',
-        // 允许 arguments.caller 或 arguments.callee
-        'no-caller': 'off',
-        // 允许 new 创建对象实例后不赋值给变量
-        'no-new': 'off',
-        // 允许不必要的转义
-        'no-useless-escape': 'off'
-    }
+  root: true,
+  env: {
+    browser: true,
+  },
+  globals: {
+    '$': false,
+    'jQuery': false,
+  },
+  extends: [
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  rules: {
+    // 不强制使用一致的缩进
+    'indent': 'off',
+    // 不强制使用一致的反勾号、双引号或单引号
+    'quotes': 'off',
+    // 不要求在语句末尾是否需要添加分号
+    'semi': 'off',
+    // 不要求一定要使用 === 和 !==
+    'eqeqeq': 'off',
+    // 不要求函数圆括号之前是否需要一个空格
+    'space-before-function-paren': 'off',
+    // 不要求语句块之前是否需要空格
+    'space-before-blocks': 'off',
+    // 不强制在关键字前后使用一致的空格
+    'keyword-spacing': 'off',
+    // 不强制在对象字面量的键和值之间使用一致的空格
+    'key-spacing': 'off',
+    // 允许行尾空格
+    'no-trailing-spaces': 'off',
+    // 允许 arguments.caller 或 arguments.callee
+    'no-caller': 'off',
+    // 允许 new 创建对象实例后不赋值给变量
+    'no-new': 'off',
+    // 允许不必要的转义
+    'no-useless-escape': 'off'
+  }
 }
--- lint.js
+++ lint.js
@@ -16,46 +16,46 @@ const lintConfigFiles = ['.eslintrc.js', '.eslintignore'];
 
 // 拷贝检测配置文件至运行检测命令的目录
 if (cwd !== initCWD) {
-    lintConfigFiles.map((val) => {
-        fs.createReadStream(val).pipe(fs.createWriteStream(path.join(initCWD, val)));
-    });
+  lintConfigFiles.map((val) => {
+    fs.createReadStream(val).pipe(fs.createWriteStream(path.join(initCWD, val)));
+  });
 }
 
 // 项目代码检测配置
 const lintConfigJson = require(initCWD + '/lint.config.json');
 
 let lintFiles = {
-    js: [],
-    vue: []
+  js: [],
+  vue: []
 };
 
 for (let obj in lintConfigJson) {
-    if (lintConfigJson.hasOwnProperty(obj)) {
-        lintConfigJson[obj].map(function (val) {
-            let fileType = val.substr(val.lastIndexOf('.') + 1);
-            let filesPath = path.join(initCWD, val);
-            if (filesPath.indexOf('!') !== -1) {
-                filesPath = '!' + filesPath.replace('!', '');
-            }
-            lintFiles[fileType] && lintFiles[fileType].push(filesPath);
-        });
-    }
+  if (lintConfigJson.hasOwnProperty(obj)) {
+    lintConfigJson[obj].map(function (val) {
+      let fileType = val.substr(val.lastIndexOf('.') + 1);
+      let filesPath = path.join(initCWD, val);
+      if (filesPath.indexOf('!') !== -1) {
+        filesPath = '!' + filesPath.replace('!', '');
+      }
+      lintFiles[fileType] && lintFiles[fileType].push(filesPath);
+    });
+  }
 }
 
 // js 代码规范检测
 gulp.task('eslint', function () {
-    return gulp.src(lintFiles.js.concat(lintFiles.vue))
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+  return gulp.src(lintFiles.js.concat(lintFiles.vue))
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 // default task
 gulp.task('default', ['eslint'], function () {
-    if (cwd !== initCWD) {
-        // 移除检测配置文件
-        lintConfigFiles.map((val) => {
-            fs.unlinkSync(path.join(initCWD, val));
-        });
-    }
+  if (cwd !== initCWD) {
+    // 移除检测配置文件
+    lintConfigFiles.map((val) => {
+      fs.unlinkSync(path.join(initCWD, val));
+    });
+  }
 });
